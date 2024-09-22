1998e9933556e5e0971a401688afad1f061a4d80
luffy <luffy@vanillafinance.xyz>
2024-09-22 19:14:27 +0800

    3.3.x.20240823.RELEASE

--- src/main/java/org/hyperic/sigar/spring/boot/SigarAutoConfiguration.java
+++ src/main/java/org/hyperic/sigar/spring/boot/SigarAutoConfiguration.java
@@ -1,9 +1,9 @@
 package org.hyperic.sigar.spring.boot;
 
-import javax.annotation.PostConstruct;
 
 import com.codahale.metrics.SharedMetricRegistries;
 import com.codahale.metrics.sigar.SigarMetrics;
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
